11efe179221f05f2a7b4cb47dc0c8c4297c3970a
robersygonzalez <robersygonzalez@gmail.com>
2026-01-17 23:15:35 -0500

    add createHashRouter

--- src/app.router.tsx
+++ src/app.router.tsx
@@ -1,5 +1,5 @@
 import { lazy } from "react";
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, createHashRouter } from "react-router";
 import { ShopLayout } from "./shop/layouts/ShopLayout";
 import { HomePage } from "./shop/pages/home/HomePage";
 import { ProductPage } from "./shop/pages/product/ProductPage";
@@ -18,7 +18,8 @@ import {
 const AuthLayout = lazy(() => import("./auth/layouts/AuthLayout"));
 const AdminLayout = lazy(() => import("./admin/layouts/AdminLayout"));
 
-export const appRouter = createBrowserRouter([
+export const appRouter = createHashRouter([
+  //export const appRouter = createBrowserRouter([
   // Main routes
   {
     path: "/",
